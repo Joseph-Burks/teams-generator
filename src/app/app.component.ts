212195b0114ee7bcaf823d198bd1356b027e5cec
Joseph-Burks <joe_burks_3@yahoo.com>
2021-11-22 14:04:04 -0600

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teams-generator';
+  title: string = 'Teams Generator';
+  newName: string = ""
+  names: string[] = []
+  errorMessage: string = ""
+  numberOfTeams: number | "" = ""
+  teams: string[][] = []
+
+  updateNewName(name:string){
+    this.newName = name
+  }
+
+  addNewName(){
+    if(!this.newName){
+      this.errorMessage = 'Name cannot be empty.'
+    } else {
+      this.names.push(this.newName)
+      this.errorMessage = ''
+      this.newName = ''
+    }
+  }
+
+  updateTeams(teams: string) {
+    this.numberOfTeams = Number(teams)
+  }
+
+  generateTeams(){
+    if(!this.numberOfTeams || this.numberOfTeams <= 0){
+      this.errorMessage = "Invalid number of Teams"
+      return
+    }
+
+    if(this.names.length < this.numberOfTeams){
+      this.errorMessage = "More teams than names"
+      return
+    }
+
+    this.errorMessage = ""
+    const allNames = [...this.names]
+
+    while(allNames.length > 0){
+      for(let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allNames.length)
+        const selectedName = allNames.splice(randomIndex, 1)[0]
+        if(!selectedName) break
+  
+        if(this.teams[i]){
+          this.teams[i].push(selectedName)
+        } else {
+          this.teams[i] = [selectedName]
+        }
+      }
+    }
+    this.names = []
+    this.numberOfTeams = ""
+  }
 }
